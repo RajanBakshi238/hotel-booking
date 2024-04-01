f13ef2b60570424a55b0ad3efc7fd9a511b351ad
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-04-01 23:46:29 +0530

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,28 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      password: '123456789',
-      username: 'postgres',
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      // password: '123456789',
+      // username: 'postgres',
+      // database: 'pgWithNest',
       entities: [User],
-      database: 'pgWithNest',
       synchronize: true,
       logging: true,
     }),
+
     UserModule,
   ],
   controllers: [AppController],
