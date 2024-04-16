316d2435fb41cf240ff276b205dc19838a150483
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-04-17 00:49:15 +0530

    api versioning done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+@Controller({ path: 'user', version: '1' })
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-
-    console.log(createUserDto,">>>>>>> create user dto" )
+    console.log(createUserDto, '>>>>>>> create user dto');
     return this.userService.create(createUserDto);
   }
 
