c4b4faafce1ffe3f8a79bc6ccc141533167596a6
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-05-06 00:02:28 +0530

    vendor entity in WIP, business entity linked

--- src/modules/business/entities/business.entity.ts
+++ src/modules/business/entities/business.entity.ts
@@ -1,10 +1,14 @@
+import { VendorDetail } from 'src/modules/vendor-details/entities/vendor-detail.entity';
 import { BaseEntity } from 'src/shared/entities/base.entity';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class BusinessEntity extends BaseEntity {
   @Column({ type: 'varchar' })
   name: string;
+
+  @OneToMany(() => VendorDetail, (vendor) => vendor.business)
+  vendors: VendorDetail[];
 
   @PrimaryGeneratedColumn()
   id: number;
--- src/modules/vendor-details/entities/vendor-detail.entity.ts
+++ src/modules/vendor-details/entities/vendor-detail.entity.ts
@@ -1,1 +1,41 @@
-export class VendorDetail {}
+import { BusinessEntity } from "src/modules/business/entities/business.entity";
+import { Column, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export class VendorDetail {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => BusinessEntity, business => business.vendors)
+    business: BusinessEntity
+
+    @Column({type: 'varchar'})
+    vendorName: string
+    
+    @Column({type: 'varchar'})
+    vendorImage: string
+    
+    @Column({type: 'varchar'})
+    vendorEmail: string
+
+    @Column({type: 'varchar'})
+    vendorAddress: string
+
+    @Column({type: 'int'})
+    noOfTables: number;
+    
+    @Column({type: 'int'})
+    seatingCapacity: number;
+
+    @Column({type: 'varchar'})
+    openingHour: string;
+
+    @Column({type: 'varchar'})
+    closingHour: string;
+
+    @Column({type: 'int'})
+    timeSlotDifference: number
+
+    @Column({type: 'boolean'})
+    isOpenSevenDays: boolean    
+
+}
